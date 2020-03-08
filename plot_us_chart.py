a70182cb626c5779453782cef434bae9c86907cb
tomerburg <tburg@albany.edu>
2020-03-08 17:46:03 -0500

    Create plot_us_chart.py

---
+++ plot_us_chart.py
@@ -0,0 +1,306 @@
+#Import packages & other scripts
+import os, sys
+import requests
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+#========================================================================================================
+# User-defined settings
+#========================================================================================================
+
+#Whether to save image or display. "directory_path" string is ignored if setting==False.
+save_image = {'setting': False,
+              'directory_path': "full_directory_path_here"}
+
+#What to plot (confirmed, deaths, recovered, active, daily)
+plot_type = "confirmed"
+
+#Include repatriated cases (e.g., cruises)?
+include_repatriated = True
+
+#Plot total numbers?
+plot_total = True
+
+#========================================================================================================
+# Get COVID-19 case data
+#========================================================================================================
+
+repatriated_locations = ['diamond princess',
+                         'grand princess']
+
+"""
+COVID-19 case data is retrieved from Johns Hopkins CSSE:
+https://github.com/CSSEGISandData/COVID-19
+"""
+
+#Avoid re-reading case data if it's already stored in memory
+try:
+    cases
+except:
+    print("--> Reading in COVID-19 case data from Johns Hopkins CSSE")
+
+    #Construct list of dates with data available, through today
+    start_date = dt.datetime(2020,1,22)
+    iter_date = dt.datetime(2020,1,22)
+    end_date = dt.datetime.today()
+    dates = []
+    dates_sites = []
+    while iter_date <= end_date:
+        strdate = iter_date.strftime("%m-%d-%Y")
+        url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{strdate}.csv'
+        request = requests.get(url)
+        if request.status_code == 200:
+            dates.append(iter_date)
+            if iter_date >= dt.datetime(2020,3,1): dates_sites.append(iter_date)
+        iter_date += dt.timedelta(hours=24)
+
+    #US states list
+    state_abbr = {
+        'AL':'Alabama',
+        'AK':'Alaska',
+        'AZ':'Arizona',
+        'AR':'Arkansas',
+        'CA':'California',
+        'CO':'Colorado',
+        'CT':'Connecticut',
+        'DE':'Delaware',
+        'DC':'District of Columbia',
+        'FL':'Florida',
+        'GA':'Georgia',
+        'HI':'Hawaii',
+        'ID':'Idaho',
+        'IL':'Illinois',
+        'IN':'Indiana',
+        'IA':'Iowa',
+        'KS':'Kansas',
+        'KY':'Kentucky',
+        'LA':'Louisiana',
+        'ME':'Maine',
+        'MD':'Maryland',
+        'MA':'Massachusetts',
+        'MI':'Michigan',
+        'MN':'Minnesota',
+        'MS':'Mississippi',
+        'MO':'Missouri',
+        'MT':'Montana',
+        'NE':'Nebraska',
+        'NV':'Nevada',
+        'NH':'New Hampshire',
+        'NJ':'New Jersey',
+        'NM':'New Mexico',
+        'NY':'New York',
+        'NC':'North Carolina',
+        'ND':'North Dakota',
+        'OH':'Ohio',
+        'OK':'Oklahoma',
+        'OR':'Oregon',
+        'PA':'Pennsylvania',
+        'RI':'Rhode Island',
+        'SC':'South Carolina',
+        'SD':'South Dakota',
+        'TN':'Tennessee',
+        'TX':'Texas',
+        'UT':'Utah',
+        'VT':'Vermont',
+        'VA':'Virginia',
+        'WA':'Washington',
+        'WV':'West Virginia',
+        'WI':'Wisconsin',
+        'WY':'Wyoming',
+    }
+
+    #Create entry for each US state, along with Diamond Princess
+    cases = {}
+    inverse_state_abbr = {v: k for k, v in state_abbr.items()}
+    for key in ['diamond princess','grand princess'] + [key.lower() for key in inverse_state_abbr.keys()]:
+        cases[key] = {'date':dates,
+                        'confirmed':[0 for i in range(len(dates))],
+                        'deaths':[0 for i in range(len(dates))],
+                        'recovered':[0 for i in range(len(dates))],
+                        'active':[0 for i in range(len(dates))],
+                        'daily':[0 for i in range(len(dates))]}
+
+    #Add individual location sites for plotting dots
+    cases_sites = {}
+    
+    #Construct list of dates
+    start_date = dates[0]
+    end_date = dates[-1]
+    while start_date <= end_date:
+
+        #Read in CSV file
+        strdate = start_date.strftime("%m-%d-%Y")
+        url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{strdate}.csv'
+        df = pd.read_csv(url)
+        df = df.fillna(0) #replace NaNs with zero
+
+        #Isolate cases to only those in US
+        df_us = df.loc[df["Country/Region"] == "US"]
+
+        #Iterate through every US case
+        for _,row in df_us.iterrows():
+
+            #Get state/province name
+            location = row['Province/State']
+
+            #Get index of date within list
+            idx = dates.index(start_date)
+            
+            #Special handling for 2/21/2020
+            if start_date.strftime("%Y%m%d") == "20200221":
+                if "Lackland, TX" in location or "Travis, CA" in location or "Ashland, NE" in location:
+                    location = location + "(Diamond Princess)"
+
+            #Handle Diamond Princess cases separately
+            if "Diamond Princess" in location:
+                cases["diamond princess"]['confirmed'][idx] += int(row['Confirmed'])
+                cases["diamond princess"]['deaths'][idx] += int(row['Deaths'])
+                cases["diamond princess"]['recovered'][idx] += int(row['Recovered'])
+                cases["diamond princess"]['active'][idx] += int(row['Confirmed']) - int(row['Recovered']) - int(row['Deaths'])
+                if idx == 0:
+                    cases["diamond princess"]['daily'][idx] = np.nan
+                else:
+                    cases["diamond princess"]['daily'][idx] = cases["diamond princess"]['confirmed'][idx] - cases["diamond princess"]['confirmed'][idx-1]
+            
+            #Handle Grand Princess cases separately
+            elif "Grand Princess" in location:
+                cases["grand princess"]['confirmed'][idx] += int(row['Confirmed'])
+                cases["grand princess"]['deaths'][idx] += int(row['Deaths'])
+                cases["grand princess"]['recovered'][idx] += int(row['Recovered'])
+                cases["grand princess"]['active'][idx] += int(row['Confirmed']) - int(row['Recovered']) - int(row['Deaths'])
+                if idx == 0:
+                    cases["grand princess"]['daily'][idx] = np.nan
+                else:
+                    cases["grand princess"]['daily'][idx] = cases["grand princess"]['confirmed'][idx] - cases["grand princess"]['confirmed'][idx-1]
+                
+            #Otherwise, handle states
+            else:
+                #Handle state abbreviation vs. full state name
+                if ',' in location:
+                    abbr = (location.split(",")[1]).replace(" ","")
+                    state = state_abbr.get(abbr)
+                else:
+                    state = str(location)
+
+                #Add cases to entry for this state & day
+                if state.lower() in cases.keys():
+                    cases[state.lower()]['confirmed'][idx] += int(row['Confirmed'])
+                    cases[state.lower()]['deaths'][idx] += int(row['Deaths'])
+                    cases[state.lower()]['recovered'][idx] += int(row['Recovered'])
+                    cases[state.lower()]['active'][idx] += int(row['Confirmed']) - int(row['Recovered']) - int(row['Deaths'])
+                    if idx == 0:
+                        cases[state.lower()]['daily'][idx] = np.nan
+                    else:
+                        cases[state.lower()]['daily'][idx] = cases[state.lower()]['confirmed'][idx] - cases[state.lower()]['confirmed'][idx-1]
+                
+                #Add individual sites
+                if start_date >= dt.datetime(2020,3,1):
+                    idx_sites = dates_sites.index(start_date)
+                    if location not in cases_sites.keys():
+                        cases_sites[location] = {'date':dates_sites,
+                            'confirmed':[0 for i in range(len(dates_sites))],
+                            'deaths':[0 for i in range(len(dates_sites))],
+                            'recovered':[0 for i in range(len(dates_sites))],
+                            'active':[0 for i in range(len(dates_sites))],
+                            'daily':[0 for i in range(len(dates_sites))],
+                            'latitude':(float(row['Latitude'])),
+                            'longitude':(float(row['Longitude']))}
+                    cases_sites[location]['confirmed'][idx_sites] = int(row['Confirmed'])
+                    cases_sites[location]['deaths'][idx_sites] = int(row['Deaths'])
+                    cases_sites[location]['recovered'][idx_sites] = int(row['Recovered'])
+                    cases_sites[location]['active'][idx_sites] = int(row['Confirmed']) - int(row['Recovered']) - int(row['Deaths'])
+                    if idx_sites == 0:
+                        cases_sites[location]['daily'][idx_sites] = np.nan
+                    else:
+                        cases_sites[location]['daily'][idx_sites] = cases_sites[location]['confirmed'][idx_sites] - cases_sites[location]['confirmed'][idx_sites-1]
+
+
+        #Increment date by 1 day
+        start_date += dt.timedelta(hours=24)
+
+#========================================================================================================
+# Create plot based on type
+#========================================================================================================
+
+#Create figure
+fig,ax = plt.subplots(figsize=(9,6),dpi=125)
+
+#Total count
+total_count = np.array([0.0 for i in cases['diamond princess']['date']])
+total_count_rp = np.array([0.0 for i in cases['diamond princess']['date']])
+
+#Iterate through every region
+sorted_keys = [y[1] for y in sorted([(np.nanmax(cases[x][plot_type]), x) for x in cases.keys()])][::-1]
+sorted_value = [y[0] for y in sorted([(np.nanmax(cases[x][plot_type]), x) for x in cases.keys()])][::-1]
+for idx,(key,value) in enumerate(zip(sorted_keys,sorted_value)):
+
+    #Special handling for Diamond Princess
+    if include_repatriated == False and key in repatriated_locations: continue
+    
+    #Total count
+    if key not in repatriated_locations: total_count += np.array(cases[key][plot_type])
+    total_count_rp += np.array(cases[key][plot_type])
+    
+    #Skip plotting if zero
+    if value == 0: continue
+    
+    #Plot type
+    if idx > 19:
+        plt.plot(cases[key]['date'],cases[key][plot_type],':',linewidth=0.5,color='k',zorder=1)
+    else:
+        mtype = '--'; zord=2
+        if np.nanmax(cases[key][plot_type]) > np.percentile(sorted_value,95): mtype = '-o'; zord=3
+
+        #Plot lines
+        plt.plot(cases[key]['date'],cases[key][plot_type],mtype,zorder=zord,label=f"{key.title()} ({cases[key][plot_type][-1]})")
+
+#Plot total count
+if plot_total == True:
+    plt.plot(cases[key]['date'],total_count,':',zorder=2,label=f'Total ({int(total_count[-1])})',color='k',linewidth=2)
+    if include_repatriated == True:
+        plt.plot(cases[key]['date'],total_count_rp,':',zorder=2,label=f'Total (+ Repatriated)\n({int(total_count_rp[-1])})',color='b',linewidth=2)
+
+#Format x-ticks
+ax.set_xticks(cases[key]['date'][::7])
+ax.set_xticklabels(cases[key]['date'][::7])
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+#Plot grid and legend
+plt.grid()
+plt.legend(loc=2,prop={'size':8})
+
+#Plot title
+title_string = {
+    'confirmed':'Cumulative COVID-19 Confirmed Cases',
+    'deaths':'Cumulative COVID-19 Deaths',
+    'recovered':'Cumulative COVID-19 Recovered Cases',
+    'active':'Daily COVID-19 Active Cases',
+    'daily':'Daily COVID-19 New Cases',
+}
+add_title = "(Non-Repatriated Cases)" if include_repatriated == False else ""
+plt.title(f"{title_string.get(plot_type)} {add_title}",fontweight='bold',loc='left')
+plt.xlabel("Date",fontweight='bold')
+plt.ylabel("Cases",fontweight='bold')
+
+#Plot attribution
+plt.title(f'Data from Johns Hopkins CSSE\nLocations with {int(np.percentile(sorted_value,95))}+ total cases labeled with dots',
+          loc='right',fontsize=8)
+if plot_type == "active":
+    plt.text(0.99,0.99,"\"Active\" cases = confirmed total - recovered - deaths",fontweight='bold',
+             ha='right',va='top',transform=ax.transAxes,fontsize=8)
+plt.text(0.27,0.98,"Top 20 locations plotted",
+             ha='left',va='top',transform=ax.transAxes,fontsize=8)
+
+#Show plot and close
+if save_image['setting'] == True:
+    savepath = os.path.join(save_image['directory_path'],f"{plot_type}_chart.png")
+    plt.savefig(savepath,bbox_inches='tight')
+else:
+    plt.show()
+plt.close()
+
+#Alert script is done
+print("Done!")
