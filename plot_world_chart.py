ebb1cdcc605a3b0b8212857f8d7155e6279266fb
tomerburg <tburg@albany.edu>
2020-03-09 09:40:08 -0500

    Update plot_world_chart.py

--- plot_world_chart.py
+++ plot_world_chart.py
@@ -161,7 +161,7 @@ title_string = {
     'active':'Daily COVID-19 Active Cases',
     'daily':'Daily COVID-19 New Cases',
 }
-add_title = "(Non-Mainland China)" if mainland_china == False else ""
+add_title = "\n(Non-Mainland China)" if mainland_china == False else ""
 plt.title(f"{title_string.get(plot_type)} {add_title}",fontweight='bold',loc='left')
 plt.xlabel("Date",fontweight='bold')
 plt.ylabel("Cases",fontweight='bold')
