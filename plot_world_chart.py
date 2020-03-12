84bdf3b066bf2ab8d81139fa2b1dae4f1fc9cee3
tomerburg <tburg@albany.edu>
2020-03-11 22:41:24 -0500

    Update plot_world_chart.py

--- plot_world_chart.py
+++ plot_world_chart.py
@@ -7,6 +7,8 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+import read_data
+
 #========================================================================================================
 # User-defined settings
 #========================================================================================================
@@ -37,67 +39,10 @@ https://github.com/CSSEGISandData/COVID-19
 try:
     cases
 except:
-    #Construct list of dates with data available, through today
-    start_date = dt.datetime(2020,1,22)
-    iter_date = dt.datetime(2020,1,22)
-    end_date = dt.datetime.today()
-    dates = []
-    while iter_date <= end_date:
-        strdate = iter_date.strftime("%m-%d-%Y")
-        url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{strdate}.csv'
-        request = requests.get(url)
-        if request.status_code == 200: dates.append(iter_date)
-        iter_date += dt.timedelta(hours=24)
-
-    #Create entry for each US state, along with Diamond Princess
-    cases = {}
-
-    #Construct list of dates
-    start_date = dates[0]
-    end_date = dates[-1]
-    while start_date <= end_date:
-
-        #Read in CSV file
-        strdate = start_date.strftime("%m-%d-%Y")
-        url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{strdate}.csv'
-        df = pd.read_csv(url)
-        df = df.fillna(0) #replace NaNs with zero
-
-        #sum by country
-        cols = (df.columns).tolist()
-        if 'Last Update' in cols: df = df.drop(columns=['Last Update'])
-        if 'Latitude' in cols: df = df.drop(columns=['Latitude'])
-        if 'Longitude' in cols: df = df.drop(columns=['Longitude'])
-        df = df.groupby('Country/Region').sum()
-
-        #Iterate through every US case
-        for location,row in df.iterrows():
-
-            #Add entry for this region if previously non-existent
-            if location.lower() not in cases.keys():
-                cases[location.lower()] = {'date':dates,
-                        'confirmed':[0 for i in range(len(dates))],
-                        'deaths':[0 for i in range(len(dates))],
-                        'recovered':[0 for i in range(len(dates))],
-                        'active':[0 for i in range(len(dates))],
-                        'daily':[0 for i in range(len(dates))]}
-
-            #Get index of date within list
-            idx = dates.index(start_date)
-
-            cases[location.lower()]['confirmed'][idx] += int(row['Confirmed'])
-            cases[location.lower()]['deaths'][idx] += int(row['Deaths'])
-            cases[location.lower()]['recovered'][idx] += int(row['Recovered'])
-            cases[location.lower()]['active'][idx] += int(row['Confirmed']) - int(row['Recovered']) - int(row['Deaths'])
-            if idx == 0:
-                cases[location.lower()]['daily'][idx] = np.nan
-            elif location.lower() == 'mainland china' and strdate == '02-13-2020':
-                cases[location.lower()]['daily'][idx] = np.nan
-            else:
-                cases[location.lower()]['daily'][idx] = cases[location.lower()]['confirmed'][idx] - cases[location.lower()]['confirmed'][idx-1]
-
-        #Increment date by 1 day
-        start_date += dt.timedelta(hours=24)
+    
+    output = read_data.read_world()
+    dates = output['dates']
+    cases = output['cases']
 
 #========================================================================================================
 # Create plot based on type
@@ -127,10 +72,12 @@ for idx,(key,value) in enumerate(zip(sorted_keys,sorted_value)):
     
     #Plot type
     if idx > 19:
-        plt.plot(cases[key]['date'],cases[key][plot_type],':',linewidth=0.5,color='k',zorder=1)
+        pass
+        #plt.plot(cases[key]['date'],cases[key][plot_type],':',linewidth=0.5,color='k',zorder=1)
     else:
         mtype = '--'; zord=2
         if np.nanmax(cases[key][plot_type]) > np.percentile(sorted_value,95): mtype = '-o'; zord=3
+        zord = 22 - idx
 
         #Handle US & UK titles
         loc = key.title()
@@ -141,7 +88,7 @@ for idx,(key,value) in enumerate(zip(sorted_keys,sorted_value)):
 
 #Plot total count
 if plot_total == True:
-    plt.plot(cases[key]['date'],total_count,':',zorder=2,label=f'Total ({int(total_count[-1])})',color='k',linewidth=2)
+    plt.plot(cases[key]['date'],total_count,':',zorder=50,label=f'Total ({int(total_count[-1])})',color='k',linewidth=2)
     if mainland_china == True: plt.plot(cases[key]['date'],total_count_row,':',zorder=2,label=f'Total ROW ({int(total_count_row[-1])})',color='b',linewidth=2)
 
 #Format x-ticks
@@ -166,6 +113,8 @@ plt.title(f"{title_string.get(plot_type)} {add_title}",fontweight='bold',loc='le
 plt.xlabel("Date",fontweight='bold')
 plt.ylabel("Cases",fontweight='bold')
 
+#plt.yscale('log')
+
 #Plot attribution
 plt.title(f'Data from Johns Hopkins CSSE\nLocations with {int(np.percentile(sorted_value,95))}+ total cases labeled with dots',
           loc='right',fontsize=8)
@@ -177,7 +126,7 @@ plt.text(0.27,0.98,"Top 20 locations plotted",
 
 #Show plot and close
 if save_image['setting'] == True:
-    savepath = os.path.join(save_image['directory_path'],f"{plot_type}_chart.png")
+    savepath = os.path.join(save_image['directory_path'],f"{plot_type}_chart_world.png")
     plt.savefig(savepath,bbox_inches='tight')
 else:
     plt.show()
